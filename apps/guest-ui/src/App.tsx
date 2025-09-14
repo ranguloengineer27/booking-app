5b7f7c3338679f9d30697c203a0dd547918bf450
ranguloengineer27 <ranguloengineer27@gmail.com>
2025-09-14 13:25:38 +0200

    feat: set up monorepo structure

---
+++ apps/guest-ui/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Guest UI</h1>
+        <p>Welcome to the guest booking interface!</p>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
